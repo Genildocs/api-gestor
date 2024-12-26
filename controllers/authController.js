869562d38b1e91c0e324737bc6f269e199b4526a
genildocs <genildocs@gmail.com>
2024-12-26 04:46:39 -0300

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,9 +55,12 @@ exports.loginUser = async (req, res) => {
       expiresIn: '1h',
     });
 
-    res
-      .status(200)
-      .json({ message: 'Login realizado com sucesso', token: token });
+    res.status(200).json({
+      status: {
+        message: 'Login realizado com sucesso',
+        token: token,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Erro ao realizar login' });
