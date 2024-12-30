f02237e651de7648aa40ab6f30877b7cc3ddb551
genildocs <genildocs@gmail.com>
2024-12-30 07:50:14 -0300

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,32 @@ exports.getUsers = async (req, res) => {
     res.status(500).json({ message: 'Erro ao buscar usuários' });
   }
 };
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao criar usuário' });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao atualizar usuário' });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao deletar usuário' });
+  }
+};
