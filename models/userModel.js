0e7a591e4833518bad9714a5dcb2dee7e10a641f
genildocs <genildocs@gmail.com>
2025-01-01 05:00:46 -0300

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,7 @@ const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   uid: { type: String },
+  nome: { type: String },
   username: { type: String, required: true, unique: true },
   email: {
     type: String,
